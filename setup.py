0a9730cb01e5d3fc0ba54b0f0593f94753e0db82
Luis Medel <luis@luismedel.com>
2023-04-09 00:24:58 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="tsid",
+    maintainer="Luis Medel",
+    maintainer_email="luis@luismedel.com",
+    description="A Python library for generating Time-Sorted Unique Identifiers (TSID)",
+    url="http://www.numpy.org",
+    license="MIT",
+    version="1.0.0",
+    packages=["tsid"],
+)
