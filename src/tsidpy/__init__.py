80b0fcb56d439d98dd671218063cf86e51edfa5c
Luis Medel <luis@luismedel.com>
2023-10-13 08:19:20 +0200

    Fix exported symbols

--- src/tsidpy/__init__.py
+++ src/tsidpy/__init__.py
@@ -1,2 +1,4 @@
 
-from .tsid import TSID, TSIDGenerator, TSID_EPOCH, TSID_CHARS
+# Seems we need this worksaround to be able to access imported symbols from outside
+# https://github.com/microsoft/pylance-release/issues/2953#issuecomment-1168408943
+from .tsid import TSID as TSID, TSIDGenerator as TSIDGenerator, TSID_EPOCH as TSID_EPOCH, TSID_CHARS as TSID_CHARS
